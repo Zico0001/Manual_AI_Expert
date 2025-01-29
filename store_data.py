aeb31563c5e04dc2881c28103db61678e168b513
Zakaria Elahmadi <89327309+Zico0001@users.noreply.github.com>
2025-01-29 01:30:07 -0500

    Update store_data.py

--- store_data.py
+++ store_data.py
@@ -1,11 +1,10 @@
-# store_data.py
 from sentence_transformers import SentenceTransformer
 import faiss
 import numpy as np
 
-def index_text(text_file_path):
+def index_text():
     model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
-    with open(text_file_path, "r") as file:
+    with open("manual_text.txt", "r") as file:
         text = file.read().split('.')
     embeddings = model.encode(text)
     index = faiss.IndexFlatL2(embeddings.shape[1])
@@ -13,8 +12,7 @@ def index_text(text_file_path):
     return index, text
 
 if __name__ == "__main__":
-    text_file_path = "manual_text.txt"
-    index, text = index_text(text_file_path)
+    index, text = index_text()
     faiss.write_index(index, "manual_index.faiss")
     with open("manual_sentences.txt", "w") as file:
         file.write("\n".join(text))
